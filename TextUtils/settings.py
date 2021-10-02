d395dfbce55a841a6096fa0bbf5eee2709c5555c
Rohu <rohitsingh24052001@gmail.com>
2021-10-02 09:17:39 +0530

    initial commit

--- TextUtils/settings.py
+++ TextUtils/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4@2-fs44m$+st^ktzpf_r@s^%c#l-m!(5+6b1vtbv(!t+o)tb+'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
